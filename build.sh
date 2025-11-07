4f2d1f645afd9ab5160f3904fc7cccdc2d1c45e0
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-11-07 20:36:59 +0000

    Update build.sh to include upptst directory in build path

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Build script for ConvNetCpp
 # Note: If you encounter Color constructor errors in PlotLib, run patch_plotlib.sh first
 if [ "$1" = "--clean" ]; then
-    umk examples,tutorial,src,$HOME/upp/bazaar,$HOME/upp/uppsrc "$1" ~/.config/u++/theide/CLANG.bm -bdsa +GUI,DEBUG_FULL "bin/${1}"
+    umk upptst,examples,tutorial,src,$HOME/upp/bazaar,$HOME/upp/uppsrc "$1" ~/.config/u++/theide/CLANG.bm -bdsa +GUI,DEBUG_FULL "bin/${1}"
 else
-    umk examples,tutorial,src,$HOME/upp/bazaar,$HOME/upp/uppsrc "$1" ~/.config/u++/theide/CLANG.bm -bds +GUI,DEBUG_FULL "bin/${1}"
+    umk upptst,examples,tutorial,src,$HOME/upp/bazaar,$HOME/upp/uppsrc "$1" ~/.config/u++/theide/CLANG.bm -bds +GUI,DEBUG_FULL "bin/${1}"
 fi
